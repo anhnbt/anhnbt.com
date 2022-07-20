00ae992756e0b8a3e1ef80430a9692f1ba5a2070
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2022-07-20 08:23:39 +0700

    refactor code: đổi sang dùng JpaRepository

--- src/main/java/com/anhnbt/blog/repository/NicknameRepository.java
+++ src/main/java/com/anhnbt/blog/repository/NicknameRepository.java
@@ -3,8 +3,8 @@ package com.anhnbt.blog.repository;
 import com.anhnbt.blog.entities.Nickname;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NicknameRepository extends PagingAndSortingRepository<Nickname, Long> {
+public interface NicknameRepository extends JpaRepository<Nickname, Long> {
     Page<Nickname> findAllByEnabled(Boolean enabled, Pageable pageable);
 }
--- src/main/java/com/anhnbt/blog/repository/PostRepository.java
+++ src/main/java/com/anhnbt/blog/repository/PostRepository.java
@@ -1,10 +1,10 @@
 package com.anhnbt.blog.repository;
 
 import com.anhnbt.blog.entities.Post;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findByPostName(String slug);
 }
