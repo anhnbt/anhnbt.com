67dd3d9f159deb868f0b88719dea60372902cb2a
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2023-07-22 15:38:39 +0700

    Hiển thị cảnh báo tiếng Việt

--- src/main/java/com/anhnbt/blog/controller/MyErrorController.java
+++ src/main/java/com/anhnbt/blog/controller/MyErrorController.java
@@ -1,6 +1,8 @@
 package com.anhnbt.blog.controller;
 
 import com.anhnbt.blog.model.MetaTag;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -8,14 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class MyErrorController implements ErrorController {
 
+    private static final Logger logger = LogManager.getLogger(MyErrorController.class);
+
     @RequestMapping("/error")
     @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleError(Model model) {
+    public String handleError(HttpServletRequest request, Model model) {
+        Exception e = (Exception) request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+        logger.error("Error: ", e);
         MetaTag metaTag = new MetaTag();
-        metaTag.setTitle("500 Internal Server Error");
+        metaTag.setTitle("Đã xảy ra lỗi");
         model.addAttribute("metaTag", metaTag);
         return "error/500";
     }
